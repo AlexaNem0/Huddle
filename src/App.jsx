a7ac7b4c9e24038dd87da492eb734251e1dac6d0
AlexaNem0 <vikalyeksyeyev@gmail.com>
2023-01-28 11:20:40 -0600

    App fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Description from "./components/Description";
 
 function App() {
   return (
-    <div className="App flex flex-col justify-center h-screen gap-[3rem]  md:gap-10 lg:gap-2 lg:mx-36  ">
+    <div className="App flex flex-col justify-center h-screen gap-4 md:gap-10 lg:gap-2 lg:mx-36  ">
       <Logo />
       <Description />
       <Footer />
